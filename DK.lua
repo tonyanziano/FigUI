36ab3aa0e7b996bf645ce00512da48e23eb54e0f
Tony <tonyanziano5@gmail.com>
2021-01-04 19:13:25 -0800

    Fixed textures and added runic power bar

--- DK.lua
+++ DK.lua
@@ -4,14 +4,22 @@ Fig.overlord = CreateFrame('FRAME', 'FigUI-Overlord', UIParent) -- frame that li
 -- event handling
 events = {};
 
+FigDK = {}
+
+function FigDK.drawRunicPower(frame)
+  local rp = UnitPower('player')
+  frame.text:SetText(''..rp)
+  frame:SetValue(rp)
+end
+
 function events:UNIT_POWER_UPDATE(...)
-  local self, unitId, resource = ...
-  if resource == 'RUNIC_POWER' then
-    -- redraw runic power
+  local unitId, resource = ...
+  if unitId == 'player' and resource == 'RUNIC_POWER' then
+    FigDK.drawRunicPower(_G['FigResourceDKRunicPower'])
   end
 end
 
-local function drawRunes()
+local function updateRunes()
   for i=1, 6 do
     local start, duration, cooledDown = GetRuneCooldown(i)
     if not cooledDown then
@@ -32,7 +40,7 @@ end
 function events:RUNE_POWER_UPDATE(...)
   local runeIndex, _ = ...
   if runeIndex == 1 then
-    drawRunes()
+    updateRunes()
   end
 end
 
@@ -42,7 +50,6 @@ end
 
 -- Register events
 for k, v in pairs(events) do
-  print('Registering event handler: '..k)
   Fig.overlord:RegisterEvent(k)
 end
 Fig.overlord:SetScript('OnEvent', handleEvents)
@@ -52,44 +59,51 @@ local function makeFrame(name, parent)
   return f
 end
 
+local function getTexturePath(texture)
+  return 'Interface\\AddOns\\FigUI-2\\Textures\\' .. texture
+end
 
-local containerWidth, containerHeight, runePaddingX, runePaddingY = 250, 50, 10, 10
-local function drawResource()
+local runePaddingX, runePaddingY = 2, 0
+function FigDK.drawRunes()
   -- positioning and creation
-  local container = makeFrame('FigRuneContainer')
-  container:SetWidth(containerWidth)
-  container:SetHeight(containerHeight)
-  container:SetPoint('CENTER', UIParent, 'CENTER', 0, -120)
+  local numRunes = 6
+  local container = _G['FigResourceDKRunes']
   container.tex = container:CreateTexture(nil, 'BACKGROUND')
   container.tex:SetAllPoints(container)
   container.tex:SetColorTexture(0, 0, 0, 0.5) -- gray
-  container:Show()
 
-  local numRunes = 6
   -- find the leftover space in the container after padding and divide it by num of runes
-  local runeWidth = (containerWidth - ((numRunes + 1) * runePaddingX)) / numRunes
+  local containerWidth, containerHeight = container:GetWidth(), container:GetHeight()
+  local runeWidth = (containerWidth - ((numRunes - 1) * runePaddingX)) / numRunes
   Fig.runeWidth = runeWidth
 
   for i=1, numRunes do
     local f = makeFrame('FigRune' .. i, container)
     f:SetHeight(containerHeight - 2 * runePaddingY)
     f:SetWidth(runeWidth)
-    f:SetPoint('LEFT', f:GetParent(), 'LEFT', (runePaddingX * i) + ((i - 1) * runeWidth), 0)
+    local prevRune = _G['FigRune' .. (i - 1)]
+    if not (prevRune == nil) then
+      -- place it in relation to the previous rune
+      f:SetPoint('LEFT', prevRune, 'LEFT', runeWidth + runePaddingX, 0)
+    else
+      -- place it in relation to the container
+      f:SetPoint('LEFT', f:GetParent(), 'LEFT')
+    end
+    
     -- bg
     f.texture = f:CreateTexture(nil, 'BACKGROUND')
     f.texture:SetAllPoints(f)
-    f.texture:SetColorTexture(77, 77, 77, 1) -- gray
+    f.texture:SetColorTexture(77/255, 77/255, 77/255, 1) -- gray
     -- fill
     f.fill = f:CreateTexture(nil, 'ARTWORK')
+    f.fill:SetVertexColor(.36, .17, .89, 1) -- yellow
+    f.fill:SetTexture(getTexturePath('GenericBarFill'))
     f.fill:SetPoint('LEFT', f, 'LEFT')
     f.fill:SetWidth(f:GetWidth())
     f.fill:SetHeight(f:GetHeight())
-    f.fill:SetColorTexture(0, 50, 0, 1) -- green
     -- animations to make the bar grow
     f.fill.animG = f.fill:CreateAnimationGroup()
     f.fill.RuneRegen = f.fill.animG:CreateAnimation('Scale')
     f.fill.RuneRegen:SetOrigin('LEFT', 0, 0)
   end
 end
-
-drawResource()
