b5760efc61d663dcf4efd2d0d97089d4a28c0c76
Tony <tonyanziano5@gmail.com>
2021-01-10 23:01:51 -0800

    Fixed DK rune UI

--- Resources/DeathKnight.lua
+++ Resources/DeathKnight.lua
@@ -1,55 +1,40 @@
--- event handling
-events = {};
-
 FigDK = {}
 
-function FigDK.drawRunicPower(frame)
+function FigDK.updateRunicPower(frame, ...)
   local rp = UnitPower('player')
-  frame.text:SetText(''..rp)
-  frame:SetValue(rp)
+  frame.runicPower.text:SetText(''..rp)
+  frame.runicPower:SetValue(rp)
 end
 
-function events:UNIT_POWER_UPDATE(...)
-  local unitId, resource = ...
-  if unitId == 'player' and resource == 'RUNIC_POWER' then
-    FigDK.drawRunicPower(_G['FigResourceDeathKnightRunicPower'])
+function FigDK.updateRunes(frame, ...)
+  for i=1, 6 do
+    local start, duration, cooledDown = GetRuneCooldown(i)
+    local runeFrame = _G['FigRune'..i]
+    runeFrame.cd:SetCooldown(start, duration)
   end
 end
 
-local function updateRunes()
-  for i=1, 6 do
-    local start, duration, cooledDown = GetRuneCooldown(i)
-    if not cooledDown then
-      -- update the bar
-      local runeFrame = _G['FigRune' ..i]
-      runeFrame.fill.RuneRegen:SetDuration(duration)
-      runeFrame.fill.RuneRegen:SetScale(Fig.runeWidth, 1)
-      runeFrame.fill:SetWidth(1)
-      -- TODO: set opacity to less?
-      runeFrame.fill.animG:Play()
-      -- TODO: on the end of the animation, reset the width to max
-    else
-      -- fill the bar
+function FigDK.handleEvents(frame, event, ...)
+  if event == 'RUNE_POWER_UPDATE' then
+    local index = ...
+    if index >= 1 and index <= 6 then
+      FigDK.updateRunes(frame, ...)
+    end
+  end
+
+  if event == 'UNIT_POWER_UPDATE' then
+    local unitId, resource = ...
+    if unitId == 'player' and resource == 'RUNIC_POWER' then
+      FigDK.updateRunicPower(frame, ...)
     end
   end
 end
 
-function events:RUNE_POWER_UPDATE(...)
-  local runeIndex, _ = ...
-  if runeIndex == 1 then
-    updateRunes()
-  end
-end
-
-local function handleEvents(self, event, ...)
-  events[event](self, ...)
-end
-
 local runePaddingX, runePaddingY = 2, 0
-function FigDK.drawRunes()
+function FigDK.drawRunes(frame)
   -- positioning and creation
   local numRunes = 6
-  local container = _G['FigResourceDeathKnightRunes']
+  local container = frame.runeContainer
   container.tex = container:CreateTexture(nil, 'BACKGROUND')
   container.tex:SetAllPoints(container)
   container.tex:SetColorTexture(0, 0, 0, 0.5) -- gray
@@ -60,7 +45,7 @@ function FigDK.drawRunes()
   Fig.runeWidth = runeWidth
 
   for i=1, numRunes do
-    local f = Fig.makeFrame('FigRune' .. i, container)
+    local f = CreateFrame('StatusBar', 'FigRune' .. i, container, 'FigDeathKnightRune')
     f:SetHeight(containerHeight - 2 * runePaddingY)
     f:SetWidth(runeWidth)
     local prevRune = _G['FigRune' .. (i - 1)]
@@ -71,29 +56,15 @@ function FigDK.drawRunes()
       -- place it in relation to the container
       f:SetPoint('LEFT', f:GetParent(), 'LEFT')
     end
-    
-    -- bg
-    f.texture = f:CreateTexture(nil, 'BACKGROUND')
-    f.texture:SetAllPoints(f)
-    f.texture:SetColorTexture(77/255, 77/255, 77/255, 1) -- gray
-    -- fill
-    f.fill = f:CreateTexture(nil, 'ARTWORK')
-    f.fill:SetVertexColor(.36, .17, .89, 1) -- yellow
-    f.fill:SetTexture(Fig.getTexturePath('GenericBarFill'))
-    f.fill:SetPoint('LEFT', f, 'LEFT')
-    f.fill:SetWidth(f:GetWidth())
-    f.fill:SetHeight(f:GetHeight())
-    -- animations to make the bar grow
-    f.fill.animG = f.fill:CreateAnimationGroup()
-    f.fill.RuneRegen = f.fill.animG:CreateAnimation('Scale')
-    f.fill.RuneRegen:SetOrigin('LEFT', 0, 0)
   end
 end
 
 function FigDK.initialize(frame)
-  -- Register events
-  for k, v in pairs(events) do
-    frame:RegisterEvent(k)
-  end
-  frame:SetScript('OnEvent', handleEvents)
+  frame:RegisterEvent('RUNE_POWER_UPDATE')
+  frame:RegisterEvent('UNIT_POWER_UPDATE')
+  frame:SetScript('OnEvent', FigDK.handleEvents)
+
+  -- TODO: initial draw?
+  FigDK.drawRunes(frame)
+  FigDK.updateRunicPower(frame)
 end
