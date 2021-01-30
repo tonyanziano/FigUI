ad870a62eaee6ea24f96d73571d39cc683d35382
Tony <tonyanziano5@gmail.com>
2021-01-29 17:39:57 -0800

    Fixed bad texture paths due to rename

--- FigUtils.lua
+++ FigUtils.lua
@@ -4,7 +4,7 @@ function Fig.makeFrame(name, parent)
 end
 
 function Fig.getTexturePath(texture)
-  return 'Interface\\AddOns\\FigUI-2\\Textures\\' .. texture
+  return 'Interface\\AddOns\\FigUI\\Textures\\' .. texture
 end
 
 function Fig.prettyPrintNumber(num)
@@ -13,5 +13,41 @@ function Fig.prettyPrintNumber(num)
     return format('%.1f', tostring(num / 1000)) .. 'k'
   else
     return tostring(num)
+  end
+end
+
+function Fig.drawBordersForFrame(frame)
+  if not frame then return end
+  
+  local borderThickness = 2
+  borderThickness = PixelUtil.GetNearestPixelSize(borderThickness, frame:GetEffectiveScale(), 1)
+  if not frame.hasBorders then
+    -- draw borders
+
+    local top = frame:CreateLine()
+    top:SetColorTexture(0, 0, 0, 1)
+    top:SetStartPoint('TOPLEFT', frame, -1, 1)
+    top:SetEndPoint('TOPRIGHT', frame, 1, 1)
+    top:SetThickness(borderThickness)
+    
+    local bot = frame:CreateLine()
+    bot:SetColorTexture(0, 0, 0, 1)
+    bot:SetStartPoint('BOTTOMLEFT', frame, -1, -1)
+    bot:SetEndPoint('BOTTOMRIGHT', frame, 1, -1)
+    bot:SetThickness(borderThickness)
+    
+    local left = frame:CreateLine()
+    left:SetColorTexture(0, 0, 0, 1)
+    left:SetStartPoint('TOPLEFT', frame, -1, 1)
+    left:SetEndPoint('BOTTOMLEFT', frame, -1, -1)
+    left:SetThickness(borderThickness)
+    
+    local right = frame:CreateLine()
+    right:SetColorTexture(0, 0, 0, 1)
+    right:SetStartPoint('TOPRIGHT', 1, 1)
+    right:SetEndPoint('BOTTOMRIGHT', 1, -1)
+    right:SetThickness(borderThickness)
+
+    frame.hasBorders = true
   end
 end
