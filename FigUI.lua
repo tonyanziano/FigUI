c8cdec7a55060738c720ae07628ea14c655b663c
Tony <tonyanziano5@gmail.com>
2021-05-28 10:32:44 -0700

    Added /figdebug slash command

--- FigUI.lua
+++ FigUI.lua
@@ -1,19 +1,6 @@
 Fig = { frames = {} }
 
-if not FigConfig then
-  print('No saved Fig config detected. Generating new table.')
-  FigConfig = { }
-else
-  print('Picked up Fig config from saved variables')
-end
-
 -- register slash commands
-SLASH_SHOW_OPTIONS1, SLASH_SHOW_OPTIONS2 = '/fig', '/figui'
-function SlashCmdList.SHOW_OPTIONS()
-  -- this function is bugged and calling it twice navigates to the correct panel
-  InterfaceOptionsFrame_OpenToCategory(FigConfigPanel)
-  InterfaceOptionsFrame_OpenToCategory(FigConfigPanel)
-end
 
 SLASH_TOGGLE_FRAME_LOCK1 = '/figlock'
 function SlashCmdList.TOGGLE_FRAME_LOCK()
@@ -23,3 +10,12 @@ function SlashCmdList.TOGGLE_FRAME_LOCK()
     end
   end
 end
+
+SLASH_TOGGLE_FIG_DEBUG1 = '/figdebug';
+function SlashCmdList.TOGGLE_FIG_DEBUG()
+  if FigDebug then
+    FigDebug.logging = not FigDebug.logging
+    print(format('Fig debug logging is now %s.', FigDebug.logging and 'enabled' or 'disabled'))
+  end
+end
+
