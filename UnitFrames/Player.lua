81bcbcef80a0f0542aa822f4acf13fdf90c1f61a
Tony <tonyanziano5@gmail.com>
2021-05-31 13:34:49 -0700

    Fixed resource bar no-op behavior

--- UnitFrames/Player.lua
+++ UnitFrames/Player.lua
@@ -45,16 +45,18 @@ function FigPlayerMixin.initialize(frame)
   -- get resource bar if applicable
   local resourceBar = FigResources.getResourceBarForPlayer()
 
-  --TODO: position the bar according to compact / pop-out mode
-  -- if config.compactResourceBar then
-  resourceBar:SetPoint('TOPLEFT', frame, 'BOTTOMLEFT', 0, -4)
-  -- else
-  -- end
+  if resourceBar then
+    --TODO: position the bar according to compact / pop-out mode
+    -- if config.compactResourceBar then
+    resourceBar:SetPoint('TOPLEFT', frame, 'BOTTOMLEFT', 0, -4)
+    -- else
+    -- end
 
-  -- size the bar
-  resourceBar:SetWidth(frame:GetWidth())
-  resourceBar:SetHeight(25)
-  
-  -- let the bar initialize
-  resourceBar:initialize()
+    -- size the bar
+    resourceBar:SetWidth(frame:GetWidth())
+    resourceBar:SetHeight(25)
+    
+    -- let the bar initialize
+    resourceBar:initialize()
+  end
 end
