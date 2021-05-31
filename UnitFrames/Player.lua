9c9820420fe0e4e82a03abd451bcb2342f08be51
Tony <tonyanziano5@gmail.com>
2021-05-31 13:03:21 -0700

    Added player combat indicator badge

--- UnitFrames/Player.lua
+++ UnitFrames/Player.lua
@@ -1,6 +1,30 @@
 FigPlayerMixin = {}
 
+local function initCombatIndicator(playerFrame)
+  playerFrame.combatIndicator = CreateFrame('frame', 'FigPlayerCombatIndicator', playerFrame, 'FigPlayerCombatIndicatorTemplate')
+  playerFrame.combatIndicator:SetPoint('TOPLEFT', playerFrame, 'TOPLEFT', 0, 0)
+  playerFrame.combatIndicator:SetScale(0.7)
+end
+
+local function onEvent(frame, event, ...)
+  if event == 'PLAYER_REGEN_DISABLED' then
+    if not frame.combatIndicator then
+      initCombatIndicator(frame)
+    end
+    frame.combatIndicator:Show()
+  elseif 'PLAYER_REGEN_ENABLED' then
+    if not frame.combatIndicator then
+      initCombatIndicator(frame)
+    end
+    frame.combatIndicator:Hide()
+  end
+end
+
 function FigPlayerMixin.initialize(frame)
+  frame:RegisterEvent('PLAYER_REGEN_DISABLED')
+  frame:RegisterEvent('PLAYER_REGEN_ENABLED')
+  frame:SetScript('OnEvent', onEvent)
+
   -- get resource bar if applicable
   local resourceBar = FigResources.getResourceBarForPlayer()
 
