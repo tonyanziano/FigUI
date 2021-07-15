a02ab3119a0c6260d82c449ba03726b3a517224c
Tony <tonyanziano5@gmail.com>
2021-07-15 16:26:14 -0700

    Fixed drag bug with player frame

--- Templates/MovableFrame.lua
+++ Templates/MovableFrame.lua
@@ -2,12 +2,16 @@ FigMovableFrameMixin = {}
 
 function FigMovableFrameMixin.moveFrame(frame)
   if not FigFramesAreLocked then
+    FigDebug.log('<FigUI> Started dragging frame: ', frame:GetName())
+    frame:ClearAllPoints()
     frame:StartMoving()
+    frame:SetUserPlaced(true)
   end
 end
 
 function FigMovableFrameMixin.stopMovingFrame(frame)
   if not FigFramesAreLocked then
+    FigDebug.log('<FigUI> Stopped dragging frame: ', frame:GetName())
     frame:StopMovingOrSizing()
   end
 end
