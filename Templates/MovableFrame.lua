e29b674a7328772612e080faf56655c8964855e5
Tony <tonyanziano5@gmail.com>
2021-07-16 12:11:16 -0700

    Added improved moving indicator to movable frames

--- Templates/MovableFrame.lua
+++ Templates/MovableFrame.lua
@@ -1,7 +1,29 @@
 FigMovableFrameMixin = { movableFrames = {} }
 
+-- create an indicator that will be shown when the frame is movable
+local function createMovableIndicator(frame)
+  frame.movableIndicator = CreateFrame('frame', frame:GetName() .. 'MovingIndicator', frame)
+  frame.movableIndicator:SetAllPoints()
+  frame.movableIndicator:SetSize(frame:GetSize())
+  frame.movableIndicator:SetFrameLevel(frame:GetFrameLevel() + 99)
+
+  -- create an overlay with some text on it
+  frame.movableIndicator.overlay = frame.movableIndicator:CreateTexture(frame.movableIndicator:GetName() .. 'Overlay')
+  frame.movableIndicator.overlay:SetAllPoints()
+  frame.movableIndicator.overlay:SetColorTexture(0, 0, 0, 0.5)
+  frame.movableIndicator.overlay:SetDrawLayer('OVERLAY')
+  frame.movableIndicator.overlay:Show()
+
+  frame.movableIndicator.text = frame.movableIndicator:CreateFontString(frame.movableIndicator:GetName() .. 'Text', 'OVERLAY', 'Game11Font')
+  frame.movableIndicator.text:SetPoint('CENTER', frame.movableIndicator, 'CENTER')
+  frame.movableIndicator.text:SetText('MOVABLE')
+
+  frame.movableIndicator:Hide()
+end
+
 function FigMovableFrameMixin.registerFrameForMovement(frame)
   if not FigMovableFrameMixin.movableFrames[frame:GetName()] then
+    createMovableIndicator(frame)
     FigMovableFrameMixin.movableFrames[frame:GetName()] = frame
   end
 end
@@ -11,11 +33,13 @@ function FigMovableFrameMixin.toggleFrameMovement()
   if FigFramesAreLocked then
     for k, v in pairs(FigMovableFrameMixin.movableFrames) do
       local frame = v
+      frame.movableIndicator:Hide()
       frame:EnableMouse(frame.mouseEnabledCopy or false)
     end
   else
     for k, v in pairs(FigMovableFrameMixin.movableFrames) do
       local frame = v
+      frame.movableIndicator:Show()
       -- store the previous mouseEnabled value to be restored when finished dragging
       frame.mouseEnabledCopy = frame:IsMouseEnabled()
       frame:EnableMouse(true)
