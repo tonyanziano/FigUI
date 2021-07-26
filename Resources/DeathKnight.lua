76a65613668fe8c2f39dcdfab873b83baa283c33
Tony <tonyanziano5@gmail.com>
2021-07-26 10:47:55 -0700

    Added death knight resource bar

--- Resources/DeathKnight.lua
+++ Resources/DeathKnight.lua
@@ -1,71 +1,141 @@
-FigDK = {}
+FigResourceDeathKnightMixin = {}
 
-function FigDK.updateRunicPower(frame, ...)
-  local rp = UnitPower('player')
-  frame.runicPower.text:SetText(''..rp)
-  frame.runicPower:SetValue(rp)
-end
+FigResourceDeathKnightMixin.powerType = 'RUNIC_POWER'
+local MAX_RUNES = 6
+local runicPowerColor = PowerBarColor[Enum.PowerType.RunicPower]
 
-function FigDK.updateRunes(frame, ...)
-  for i=1, 6 do
-    local start, duration, cooledDown = GetRuneCooldown(i)
-    local runeFrame = _G['FigRune'..i]
-    runeFrame.cd:SetCooldown(start, duration)
+local function setBorders(frame)
+  if frame.leftBorder then
+    frame.leftBorder:SetStartPoint('BOTTOMLEFT')
+    frame.leftBorder:SetEndPoint('TOPLEFT')
+  end
+  if frame.rightBorder then
+    frame.rightBorder:SetStartPoint('BOTTOMRIGHT')
+    frame.rightBorder:SetEndPoint('TOPRIGHT')
+  end
+  if frame.topBorder then
+    frame.topBorder:SetStartPoint('TOPLEFT')
+    frame.topBorder:SetEndPoint('TOPRIGHT')
+  end
+  if frame.bottomBorder then
+    frame.bottomBorder:SetStartPoint('BOTTOMLEFT')
+    frame.bottomBorder:SetEndPoint('BOTTOMRIGHT')
   end
 end
 
-function FigDK.handleEvents(frame, event, ...)
-  if event == 'RUNE_POWER_UPDATE' then
-    local index = ...
-    if index >= 1 and index <= 6 then
-      FigDK.updateRunes(frame, ...)
-    end
+local runeColors = {
+  Blood = { r = 209/255, g = 21/255, b = 21/255 },
+  Frost = { r = 98/255, g = 206/255, b = 240/255 },
+  Unholy = { r = 132/255, g = 227/255, b = 54/255 }
+}
+
+local function colorRunes()
+  local currentSpec = GetSpecialization()
+  local runeColor
+  if currentSpec then
+     local _, currentSpecName = GetSpecializationInfo(currentSpec)
+     runeColor = runeColors[currentSpecName]
+
+  else
+    runeColor = runeColors.Unholy
   end
 
-  if event == 'UNIT_POWER_UPDATE' then
-    local unitId, resource = ...
-    if unitId == 'player' and resource == 'RUNIC_POWER' then
-      FigDK.updateRunicPower(frame, ...)
-    end
-  end
-end
-
-local runePaddingX, runePaddingY = 2, 0
-function FigDK.drawRunes(frame)
-  -- positioning and creation
-  local numRunes = 6
-  local container = frame.runeContainer
-  container.tex = container:CreateTexture(nil, 'BACKGROUND')
-  container.tex:SetAllPoints(container)
-  container.tex:SetColorTexture(0, 0, 0, 0.5) -- gray
-
-  -- find the leftover space in the container after padding and divide it by num of runes
-  local containerWidth, containerHeight = container:GetWidth(), container:GetHeight()
-  local runeWidth = (containerWidth - ((numRunes - 1) * runePaddingX)) / numRunes
-  Fig.runeWidth = runeWidth
-
-  for i=1, numRunes do
-    local f = CreateFrame('StatusBar', 'FigRune' .. i, container, 'FigDeathKnightRune')
-    f:SetHeight(containerHeight - 2 * runePaddingY)
-    f:SetWidth(runeWidth)
-    f.cd:SetCountdownFont('FigFontInvis')
-    local prevRune = _G['FigRune' .. (i - 1)]
-    if not (prevRune == nil) then
-      -- place it in relation to the previous rune
-      f:SetPoint('LEFT', prevRune, 'LEFT', runeWidth + runePaddingX, 0)
-    else
-      -- place it in relation to the container
-      f:SetPoint('LEFT', f:GetParent(), 'LEFT')
-    end
+  for i = 1, MAX_RUNES do
+    local rune = _G['FigRune' .. i]
+    rune:SetStatusBarColor(runeColor.r, runeColor.g, runeColor.b, 1)
   end
 end
 
-function FigDK.initialize(frame)
+function FigResourceDeathKnightMixin.doInitialDraw(frame)
+  local playerFrame = _G['FigPlayer']
+  local playerFrameWidth = playerFrame:GetWidth()
+  frame:SetSize(playerFrameWidth, 23)
+
+  -- runic power
+  frame.runicPower:SetWidth(playerFrameWidth)
+  frame.runicPower:SetHeight(15)
+  local runicPower = UnitPower('player', Enum.PowerType.RunicPower)
+  frame.runicPower.text:SetText(runicPower)
+  frame.runicPower:SetValue(runicPower)
+  frame.runicPower:SetStatusBarColor(runicPowerColor.r, runicPowerColor.g, runicPowerColor.b, 1)
+  frame.runicPower:SetPoint('BOTTOMLEFT', frame, 'BOTTOMLEFT')
+  setBorders(frame.runicPower)
+
+  -- rune container
+  local runeContainerWidth, runeContainerHeight = playerFrameWidth, 8
+  local runeWidth = runeContainerWidth / MAX_RUNES
+  frame.runeContainer:SetWidth(runeContainerWidth)
+  frame.runeContainer:SetHeight(runeContainerHeight)
+  frame.runeContainer:SetPoint('TOPLEFT', frame, 'TOPLEFT')
+  setBorders(frame.runeContainer)
+
+  -- runes
+  for i = 1, MAX_RUNES do
+    local rune = _G['FigRune' .. i]
+    rune:SetWidth(runeWidth)
+    rune:SetHeight(runeContainerHeight)
+    local xOffset = (i - 1) * runeWidth
+    rune:SetPoint('LEFT', frame.runeContainer, 'LEFT', xOffset, 0)
+  end
+  colorRunes()
+end
+
+-- update runic power
+function FigResourceDeathKnightMixin.updateResource(frame)
+  local runicPower, maxRunicPower = UnitPower('player', Enum.PowerType.RunicPower), UnitPowerMax('player', Enum.PowerType.RunicPower)
+  frame.runicPower:SetMinMaxValues(0, maxRunicPower)
+  frame.runicPower:SetValue(runicPower)
+  frame.runicPower.text:SetText(runicPower)
+end
+
+local function updateRunes()
+  for i = 1, MAX_RUNES do
+    local start, duration, runeReady = GetRuneCooldown(i)
+    local rune = _G['FigRune' .. i]
+    rune.startTime = start
+    rune.duration = duration
+    rune.isReady = runeReady
+  end
+end
+
+local function onEvent(frame, event, ...)
+  if event == 'ACTIVE_TALENT_GROUP_CHANGED' then
+    colorRunes()
+  elseif event == 'RUNE_POWER_UPDATE' then
+    updateRunes()
+  end
+end
+
+function FigResourceDeathKnightMixin.initializeRuneEvents(frame)
+  frame:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
   frame:RegisterEvent('RUNE_POWER_UPDATE')
-  frame:RegisterEvent('UNIT_POWER_UPDATE')
-  frame:SetScript('OnEvent', FigDK.handleEvents)
+  frame:HookScript('OnEvent', onEvent)
+end
 
-  -- TODO: initial draw?
-  FigDK.drawRunes(frame)
-  FigDK.updateRunicPower(frame)
+FigDeathKnightRuneMixin = {}
+
+function FigDeathKnightRuneMixin.updateRune(frame)
+  if frame.isReady or frame.startTime == nil then
+    frame:SetValue(1)
+    frame.cd:Hide()
+  else
+    -- calculate the percentage of the cooldown elapsed
+    local now = GetTime()
+    local startTime, duration = frame.startTime, frame.duration
+    local elapsed = now - startTime
+    local remaining = math.ceil(duration - elapsed)
+    local percentElapsed = elapsed / duration
+    frame:SetValue(percentElapsed)
+    frame.cd:SetText(remaining)
+    frame.cd:Show()
+  end
+end
+
+function FigDeathKnightRuneMixin.initialize(frame)
+  local start, duration, runeReady = GetRuneCooldown(frame.runeIndex)
+  frame.startTime = start
+  frame.duration = duration
+  frame.isReady = runeReady
+
+  setBorders(frame)
 end
--- Resources/FigResources.lua
+++ Resources/FigResources.lua
@@ -3,7 +3,10 @@ FigResources = {}
 function FigResources.getResourceBarForPlayer()
   local class = UnitClass('player')
 
-  if class == 'Paladin' then
+  if class == 'Death Knight' then
+    local f = CreateFrame('frame', 'FigResourceDeathKnight', UIParent, 'FigResourceDeathKnightTemplate')
+    return f
+  elseif class == 'Paladin' then
     local f = CreateFrame('frame', 'FigResourcePaladin', UIParent, 'FigResourcePaladinTemplate')
     return f
   elseif class == 'Rogue' then
